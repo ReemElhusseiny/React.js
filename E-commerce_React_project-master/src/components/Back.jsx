2f08f8598e2496e46ad7dac474e25df8b85bcd13
ReemElhusseiny <128598999+ReemElhusseiny@users.noreply.github.com>
2023-09-03 23:19:16 +0300

    Delete E-commerce_React_project-master directory

--- E-commerce_React_project-master/src/components/Back.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-// export function Back() {
-//   return (
-//     <div style={{marginTop:"10px"}}>
-//       <img src='in.jpg' style={{width:"98vw",height:"80vh"}}></img>
-//     </div>
-//       )
-// }
